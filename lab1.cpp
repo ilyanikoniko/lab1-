a741c34122986bb9865099d34166cb1e5c91530e
ilyanikoniko <164339208+ilyanikoniko@users.noreply.github.com>
2024-11-01 23:05:38 +0300

    Add files via upload
    Написал поля класса векторов, геттеры, три конструктора: по умолчанию и два с параметрами. Первый с параметрами заполняет весь массив одним числом, второй заполняет массив рандомными числами

---
+++ lab1.cpp
@@ -0,0 +1,53 @@
+﻿using namespace std;
+
+#include <iostream>
+#include <random>
+template <typename T>
+class Vector {
+
+private:
+    T* _data;          
+    size_t _size;      
+    size_t _capacity;
+public:
+    size_t get_size() const 
+    {
+        return _size;
+    }
+
+    size_t get_capacity() const
+    {
+        return _capacity;
+    }
+
+    Vector(): size(0), capacity(1) 
+    {  
+        data = new T[capacity];   
+    }
+
+    Vector(size_t size, const T& value): _size(size), _capacity(size)
+    {
+        data = new T[_capacity];
+        for (size_t i = 0; i < size; ++i)
+        {
+            data[i] = value;
+        }
+    }
+
+    Vector(size_t size, T min, T max): _size(size), _capacity(_size)
+    {
+        data = new T[_capacity];
+        /*Создание генератора случайных чисел
+         Использует аппаратный генератор
+         Инициализирует генератор*/ 
+        std::random_device rd; 
+        std::mt19937 gen(rd()); 
+        std::uniform_int_distribution<T> dist(min, max); 
+
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = dist(gen); 
+        }
+    }
+};
+
+
