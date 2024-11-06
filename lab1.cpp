1311b15d41761dad54c89fd52a868d48d2e6d98a
ilyanikoniko <164339208+ilyanikoniko@users.noreply.github.com>
2024-11-06 22:20:10 +0300

    Add files via upload
    Добавил оператор чтения по индексу, операторы сложения и вычитания векторов, оператор скалярного произведения, умножения на скаляр( с коммутативностью) и деления на скаляр

--- lab1.cpp
+++ lab1.cpp
@@ -15,7 +15,6 @@ public:
         return _size;
     }
 
-    
     Vector(size_t size, T value): _size(size)
     {
         if (_size <= 0)
@@ -44,6 +43,99 @@ public:
         }
     }
 
+    Vector(const Vector& other) : _size(other._size)
+    {
+        data = new T[_size]
+            for (size_t i = 0, i < _size, ++i)
+            {
+                data[i] = other.data[i];
+            }
+    }
+
+    ~Vector() 
+    {
+        delete[] data;
+    }
+
+    T& operator[] (size_t index) const
+    {
+        if (index >= _size || index < _size) {
+            throw std::out_of_range("Индекс выходит за пределы вектора");
+        }
+        return data[index];  
+    }
+
+    Vector operator+ (const Vector& other) const
+    {
+        if (_size != other._size)
+        {
+            throw std::invalid_argument("Вектора должны быть одинаковой размерности");
+        }
+        Vector result(_size, 0);
+        for (size_t i = 0, i < _size, ++i)
+        {
+            result[i] = data[i] + other.data[i];
+        }
+        return result;
+    }
+
+    Vector operator- (const Vector& other) const
+    {
+        if (_size != other._size)
+        {
+            throw std::invalid_argument("Вектора должны быть одинаковой размерности");
+        }
+        Vector result(_size, 0);
+        for (size_t i = 0, i < _size, ++i)
+        {
+            result[i] = data[i] - other.data[i];
+        }
+        return result;
+    }
+
+    T operator* (const Vector& other) const
+    {
+        if (_size != other._size)
+        {
+            throw std::invalid_argument("Вектора должны быть одинаковой размерности");
+        }
+        T result = 0;
+        for (size_t i = 0, i < _size, ++i)
+        {
+            result += data[i] * other.data[i];
+        }
+        return result;
+    }
+
+    Vector operator* (T val) const
+    {
+        Vector result(_size, 0);
+        for (size_t i = 0, i < _size, ++i)
+        {
+            result[i] = val * data[i];
+        }
+        return result;
+    }
+
+    friend Vector operator*(T val, const Vector& other) //коммутативность для умножения вектора на скаляр
+    {
+        return other * val;  
+    }
+
+    Vector operator/ (T val) const
+    {
+        if (val == 0) 
+        {
+            throw std::invalid_argument("Деление на ноль невозможно");
+        }
+
+        Vector result(_size, 0);
+        for (size_t i = 0, i < _size, ++i)
+        {
+            result[i] = data[i]/val;
+        }
+        return result;
+    }
 };
 
 
