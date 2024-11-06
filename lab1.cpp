590d4a6f1a8de8935c9887212b39d5ce8f3b3e1c
ilyanikoniko <164339208+ilyanikoniko@users.noreply.github.com>
2024-11-06 20:29:47 +0300

    Add files via upload
    Исправил логику конструкторов на правильную, убрал capacity

--- lab1.cpp
+++ lab1.cpp
@@ -3,40 +3,35 @@
 #include <iostream>
 #include <random>
 template <typename T>
+
 class Vector {
 
 private:
-    T* _data;          
-    size_t _size;      
-    size_t _capacity;
+    T* data;       // Указатель на данные (координаты вектора)
+    size_t _size;  // Размерность (количество координат)
 public:
     size_t get_size() const 
     {
         return _size;
     }
 
-    size_t get_capacity() const
+    
+    Vector(size_t size, T value): _size(size)
     {
-        return _capacity;
-    }
-
-    Vector(): size(0), capacity(1) 
-    {  
-        data = new T[capacity];   
-    }
-
-    Vector(size_t size, const T& value): _size(size), _capacity(size)
-    {
-        data = new T[_capacity];
+        if (_size <= 0)
+        {
+            throw std::invalid_argument("Размерность вектора должна быть больше нуля");
+        }
+        data = new T[_size];
         for (size_t i = 0; i < size; ++i)
         {
             data[i] = value;
         }
     }
 
-    Vector(size_t size, T min, T max): _size(size), _capacity(_size)
+    Vector(size_t size, T min, T max): _size(size)
     {
-        data = new T[_capacity];
+        data = new T[_size];
         /*Создание генератора случайных чисел
          Использует аппаратный генератор
          Инициализирует генератор*/ 
@@ -48,6 +43,7 @@ public:
             data[i] = dist(gen); 
         }
     }
+
 };
 
 
